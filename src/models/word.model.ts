22b02fb2edd120ab1542daab56885188a418e26b
Daniel <itzdanielmichael@gmail.com>
2022-02-22 22:48:51 -0500

    init db stuff

--- src/models/word.model.ts
+++ src/models/word.model.ts
@@ -11,3 +11,25 @@ function Word(word: TWord) {
   this.createdDateTime = word.createdDateTime;
   this.createdBy = word.createdBy;
 }
+
+Word.create = async (newWord: TWord) => {
+  try {
+    const query = `INSERT INTO Word (textContent, createdBy) VALUES (?, ?)`;
+
+    const [{ insertId }]: any = await pool.query(query, [
+      newWord.textContent,
+      newWord.createdBy,
+    ]);
+    return { id: insertId, ...newWord };
+  } catch (error) {
+    throw error;
+  }
+};
+
+Word.getAll = async (word: TWord) => {
+  let query = 'SELECT * FROM word';
+
+  const [result]: any = await pool.query(query);
+
+  return result;
+};

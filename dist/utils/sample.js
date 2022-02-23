11d01170029d9a08c8e30eae4b57fea783a09e2c
Daniel <itzdanielmichael@gmail.com>
2022-02-22 19:27:18 -0500

    update gitignore

--- dist/utils/sample.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const sample = (words) => {
-    return words[Math.floor(Math.random() * words.length)];
-};
-exports.default = sample;
-//# sourceMappingURL=sample.js.map

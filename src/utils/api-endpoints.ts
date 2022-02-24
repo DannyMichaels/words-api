71a53aac182509c5a5186d31cb39153dd8dccb80
Daniel <itzdanielmichael@gmail.com>
2022-02-23 23:05:41 -0500

    update styling

--- src/utils/api-endpoints.ts
+++ src/utils/api-endpoints.ts
@@ -1,6 +1,20 @@
 export default [
-  '/api/words/all',
-  '/api/words/random',
-  '/api/words/random?count={{count}}',
-  '/api/words/{{id}}',
+  {
+    route: '/api/word',
+    description: 'get one random word!',
+  },
+  {
+    route: '/api/words/all',
+    description: 'get all words in the database!',
+  },
+  {
+    route: '/api/words/random?count={{count}}',
+    example: '/api/words/random?count=2',
+    description: 'get random words!',
+  },
+  {
+    example: '/api/words/6',
+    route: '/api/words/{{id}}',
+    description: 'find a word by id (but why?)',
+  },
 ];

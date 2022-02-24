43fd24abd1574a3df58b90fa012cab260e954c22
Daniel <itzdanielmichael@gmail.com>
2022-02-23 19:57:48 -0500

    add endpoint ssection

--- src/controllers/web/home.controllers.ts
+++ src/controllers/web/home.controllers.ts
@@ -1,4 +1,5 @@
 import { Response, Request } from 'express';
+import API_ENDPOINTS from '../../utils/api-endpoints';
 
 export const index = async (req: Request, res: Response) => {
   try {
@@ -9,6 +10,9 @@ export const index = async (req: Request, res: Response) => {
       // the mouse partial
       mousePartialText: 'Scroll for more',
       mousePartialClickScrollSection: '#endpoints',
+
+      // api-endpoint list items
+      API_ENDPOINTS,
     });
   } catch (error) {
     return res.status(500).json({ error: error.message });
---
+++ src/utils/api-endpoints.ts
@@ -0,0 +1,6 @@
+export default [
+  '/api/words/all',
+  '/api/words/random',
+  '/api/words/random?count={{count}}',
+  '/api/words/{{id}}',
+];
